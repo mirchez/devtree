b16ee30395286441759f6b705aa14e0d54820fe1
Miguel <mmirandasanchez16@gmail.com>
2025-03-02 08:34:47 -0300

    Add api to update profile and api to upload a profile picture

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -75,7 +75,7 @@ export const getUser = async (req: Request, res: Response) => {
 
 export const updateProfile = async (req: Request, res: Response) => {
   try {
-    const { description } = req.body;
+    const { description, links } = req.body;
     const handle = slugify(req.body.handle, "");
     const handleExists = await User.findOne({ handle });
     if (handleExists && handleExists.email !== req.user.email) {
@@ -85,6 +85,7 @@ export const updateProfile = async (req: Request, res: Response) => {
     //update user
     req.user.description = description;
     req.user.handle = handle;
+    req.user.links = links;
     await req.user.save();
     res.send("user profile updated");
   } catch (e) {
